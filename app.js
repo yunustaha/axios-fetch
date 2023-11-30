b6b461019738874e1ab83d8615624db1279ca90d
yunustahayilmaz <yunustahayilmaz@gmail.com>
2023-11-30 21:10:43 +0300

    comment lines added.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import getData from "./service.js";
 
+//node app.js
 const data = await getData(1);
 
 console.log(data);
--- service.js
+++ service.js
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 export default async function getData (userId) {
     try {
+        //get user request
         const getResponse = await axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`);
+        //post user request
         const postResponse = await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`);
 
         return { ...getResponse.data, posts: postResponse.data }
